deaeeaf3adf654a86231887665dc6bbd7e24a10f
tiaan-jonker <tiaanj06@gmail.com>
2022-07-13 11:12:58 +1200

    Mutations to add, delete and edit user

--- schema/schema.js
+++ schema/schema.js
@@ -27,8 +27,11 @@ const {
   GraphQLInt,
   GraphQLSchema,
   GraphQLList,
+  GraphQLNonNull,
 } = graphql
 const axios = require('axios')
+
+// * ------------ QUERIES
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
@@ -100,6 +103,57 @@ fragment userDetails on User {
 }
 */
 
+// * ------------ MUTATIONS
+
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: { type: GraphQLNonNull(GraphQLString) },
+        age: { type: GraphQLNonNull(GraphQLString) },
+        companyId: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, { firstName, age }) {
+        return axios
+          .post(`http://localhost:3000/users`, {
+            firstName,
+            age,
+          })
+          .then((res) => res.data)
+      },
+    },
+
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, { id }) {
+        return axios
+          .delete(`http://localhost:3000/users/${id}`)
+          .then((res) => res.data)
+      },
+    },
+
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLString },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args)
+          .then((res) => res.data)
+      },
+    },
+  },
+})
+
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
@@ -126,4 +180,5 @@ const RootQuery = new GraphQLObjectType({
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation,
 })
